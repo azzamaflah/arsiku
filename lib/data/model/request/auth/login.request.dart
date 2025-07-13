3cbcf75e439e69bef3c8c71f0157ca662fce53df
azzamaflah <azzamhebat46@gmail.com>
2025-07-13 22:08:19 +0700

    untuk login terus ada validasinya juga

---
+++ lib/data/model/request/auth/login.request.dart
@@ -0,0 +1,12 @@
+// lib/data/model/request/auth/login.request.dart
+
+class LoginRequest {
+  final String email;
+  final String password;
+
+  LoginRequest({required this.email, required this.password});
+
+  Map<String, dynamic> toMap() {
+    return {'email': email, 'password': password};
+  }
+}
